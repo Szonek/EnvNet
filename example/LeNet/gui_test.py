dc04b173d30dde45c98852253f08cf180777ab68
Szymon M <szymonpl17@gmail.com>
2018-12-21 11:31:59 +0100

    more lenet work

--- example/LeNet/gui_test.py
+++ example/LeNet/gui_test.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
+import scipy.ndimage.interpolation as sp
+import example.LeNet.lenet
+
+network = example.LeNet.lenet.LeNet()
 
 drawing = False # true if mouse is pressed
 mode = True # if True, draw rectangle. Press 'm' to toggle to curve
 ix,iy = -1,-1
-
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
     global ix,iy,drawing,mode
@@ -15,21 +18,28 @@ def draw_circle(event,x,y,flags,param):
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
-            cv2.circle(img,(x,y),5,(0,0,0),-1)
+            cv2.circle(img,(x,y),2,(255,255,255),-1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        cv2.circle(img,(x,y),5,(0,0,0),-1)
+        cv2.circle(img,(x,y),2,(255,255,255),-1)
 
-img = np.full((512,512,1), 255, np.uint8)
+img = np.zeros((128,128,1), np.float)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
+
 
 while(1):
     cv2.imshow('image',img)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
-        img = np.full((512, 512, 1), 255, np.uint8)
+        img = np.zeros((128, 128, 1), np.float)
+    if k == ord('a'):
+        img_to_network = sp.zoom(img, (1/4, 1/4, 1))
+        img_to_network /= 255
+        network.set_input(img_to_network)
+        out = network.execute()
+        print('end')
     elif k == 27:
         break
 
