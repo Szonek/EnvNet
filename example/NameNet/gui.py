b2aa3356a966bf60813b2ff8c01a52fe3a7d92d9
Szymon M <szymonpl17@gmail.com>
2019-01-01 16:21:47 +0100

    done

--- example/NameNet/gui.py
+++ example/NameNet/gui.py
@@ -8,14 +8,35 @@ class Gui:
     def __init__(self, network):
         self.network = network
         self.master = Tk()
+        self.__configure_window()
         self.last_name = Entry(self.master)
-        self.last_name.grid(row=0, column=1)
-        Label(self.master, text="Last name:").grid(row=0)
-        Button(self.master, text="Predict", command=self.__predict).grid(row=3, column=0, sticky=W, pady=4)
+        self.last_name.place(x=200, y=50)
+        Label(self.master, text="Last name:", font="Verdana 10").place(x=100, y=50)
+        Button(self.master, text="Predict", command=self.__predict,width=15,height=4).place(x=100, y=100)
+        self.result_0 = Label(self.master, text="0:", font="Verdana 10")
+        self.result_1 = Label(self.master, text="1:", font="Verdana 10")
+        self.result_2 = Label(self.master, text="2:", font="Verdana 10")
+        self.__configure_pred_lables()
         pass
 
+    def __configure_window(self):
+        self.master.title("Predict last name")
+        self.master.geometry("450x300")
+        self.master.resizable(0, 0)
+
+    def __configure_pred_lables(self):
+        self.result_0.place(x=230, y=100)
+        self.result_1.place(x=230, y=120)
+        self.result_2.place(x=230, y=140)
+
     def __predict(self):
-        input = self.network.word_to_tensor(self.last_name.get())
+        name = self.last_name.get()
+        if len(name) == 0:
+            self.result_0.configure(text="0:")
+            self.result_1.configure(text="1:")
+            self.result_2.configure(text="2:")
+            return
+        input = self.network.word_to_tensor(name)
         hidden = np.zeros((1, 128))
         for i in range(input.shape[0]):
             self.network.set_input(input[i])
@@ -23,12 +44,9 @@ class Gui:
             out = self.network.execute()
             out, hidden = out["output"], out["i2h"]
         sort_idx = np.argsort(out)
-        category_i = sort_idx[0][0][0][-1]
-        print(self.network.categories[category_i], category_i)
-        category_i = sort_idx[0][0][0][-2]
-        print(self.network.categories[category_i], category_i)
-        category_i = sort_idx[0][0][0][-3]
-        print(self.network.categories[category_i], category_i)
+        self.result_0.configure(text="0:" + self.network.categories[sort_idx[0][0][0][-1]])
+        self.result_1.configure(text="1:" + self.network.categories[sort_idx[0][0][0][-2]])
+        self.result_2.configure(text="2:" + self.network.categories[sort_idx[0][0][0][-3]])
 
     def run(self):
         mainloop()
